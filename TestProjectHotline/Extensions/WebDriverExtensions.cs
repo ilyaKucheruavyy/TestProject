44d21ec7b93d7b398f99873e44d398c136e3dfa4
Ilya Kucheryavyy <captain34690@gmail.com>
2022-08-30 17:34:52 +0300

    Remove comments

--- TestProjectHotline/Extensions/WebDriverExtensions.cs
+++ TestProjectHotline/Extensions/WebDriverExtensions.cs
@@ -20,8 +20,6 @@ namespace TestProjectHotline.Extensions
         {
             var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(15));
             wait.Until(ElementIsInDisplayedCondition(element, true));
-            // тут, антил, будет ждать 15 секунд, бо так сетнуто выше в вебДрайверВейт, на предмет того, что елемент все таки откинет тру, ну то есть будет виден
-            // если закинуть фолс, метод будет ждать, что в течении 15 секунд, елемент пропадёт
         }
 
         public static void WaitUntilTextToBePresent(this IWebDriver driver, IWebElement element, string text)
@@ -34,21 +32,18 @@ namespace TestProjectHotline.Extensions
         {
             var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(15));
             wait.Until(ExpectedConditions.ElementIsVisible(by));
-            // я бы использовал в этом проекте этот вейтер, бо если честно, редко бывают ситуации, когда ты получил уже вебЕлемент, но он меняет свой дисплейд стейтмент
         }
 
-        private static Func<IWebDriver, bool> ElementIsInDisplayedCondition(IWebElement element, bool displayedCondition) // для драйвера здорово 
+        private static Func<IWebDriver, bool> ElementIsInDisplayedCondition(IWebElement element, bool displayedCondition) 
         {
             return (driver) =>
             {
                 try
                 {
-                    // тут короче прокинул ретур стейт, который тупо чекает, елемент дисплейд или нет здарова ебать
                     return element.Displayed.Equals(displayedCondition);
                 }
                 catch (Exception e)
                 {
-                    // собсна если нет, то вернется фолс
                     return false.Equals(displayedCondition);
                 }
             };
